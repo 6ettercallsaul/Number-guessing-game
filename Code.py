d82af7f85b1d584d56201447b64f5a1832dae167
Dinesh <dineshpost0@gmail.com>
2026-03-03 22:15:20 +0530

    feat: added limited attempts

--- Code.py
+++ Code.py
@@ -1,12 +1,14 @@
-hereimport random
+import random
 
 print("Welcome to the Number Guessing Game!")
 print("I am thinking of a number between 1 and 100.")
+print("You have 7 attempts to guess the number.")
 
 number = random.randint(1, 100)
 attempts = 0
+max_attempts = 7
 
-while True:
+while attempts < max_attempts:
     guess = int(input("Enter your guess: "))
     attempts += 1
 
@@ -18,3 +20,6 @@ while True:
         print("Congratulations! You guessed it.")
         print("Number of attempts:", attempts)
         break
+else:
+    print("\nGame Over! You've used all your attempts.")
+    print("The correct number was:", number)
